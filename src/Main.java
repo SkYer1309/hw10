bd2bc19d252260857c8c2fdb97ea0d5eab0b682e
skyer <sky13net84@gmail.com>
2026-03-02 09:11:13 +0300

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,13 @@ import java.util.stream.IntStream;
 public class Main {
     public static void main(String[] args) {
         System.out.println("\nЗадача 1");
+        String firstName = "Ivanov ";
+        String middleName = "Ivan ";
+        String lastName = "Ivanovich ";
+        String foolName = (firstName + middleName + lastName);
+        System.out.println(foolName);
+
+        System.out.println("\nЗадача 2");
 
     }
 }
